f550e988103989a216c0dd4ecd4b03161a6e1ac3
JoshuaMukherjee <joshuaakmukherjee@gmail.com>
2020-12-10 13:31:42 +0000

    url

--- Django/web_project/settings.py
+++ Django/web_project/settings.py
@@ -25,15 +25,15 @@ SECRET_KEY = '2gult1d96#@#b2%tz+k9x1q%-4(%f@va-!sbv*q&$t^gpp8-_='
 # SECURITY WARNING: don't run with debug turned on in production!
 # If you set to False, also add "localhost" to ALLOWED_HOSTS or else
 # you'll get "Bad Request" when running locally.
-DEBUG = False
+DEBUG = True
 
 # When deploying to Azure App Service, add you <name>.azurewebsites.net 
 # domain to ALLOWED_HOSTS; you get an error message if you forget. When you add
 # a specific host, you must also add 'localhost' and/or '127.0.0.1' for local
 # debugging (which are enabled by default when ALLOWED_HOSTS is empty.)
 ALLOWED_HOSTS = [
-    'localhost',
-    'https://django-knhjzaotmvmng.azurewebsites.net'
+    #'localhost',
+    #'https://django-knhjzaotmvmng.azurewebsites.net'
     #'127.0.0.1'
     #'vscode-django-tutorial.azurewebsites.net'  # Sample name only!
 ]
