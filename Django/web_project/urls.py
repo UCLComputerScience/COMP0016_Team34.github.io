4e103fe85d426a7c0641c00d0ff8f34590720ddf
JoshuaMukherjee <joshuaakmukherjee@gmail.com>
2020-12-10 14:07:24 +0000

    Hello

--- Django/web_project/urls.py
+++ Django/web_project/urls.py
@@ -17,12 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import include, path
-from web_project import views
 
 urlpatterns = [
     path("", include("hello.urls")),
-    #path("admin/", admin.site.urls),  # Activates the admin interface
-    path("time/",views.current_datetime)
+    path("admin/", admin.site.urls),  # Activates the admin interface
 ]
 
 urlpatterns += staticfiles_urlpatterns()
