dbbd04625d2c48183c3a8f6e1219d0b051f6127c
JoshuaMukherjee <joshuaakmukherjee@gmail.com>
2020-12-17 16:28:26 +0000

    static files

--- Django/azuresite/settings.py
+++ Django/azuresite/settings.py
@@ -118,5 +118,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/azuresite/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [BASE_DIR/"azuresite/static/",]
