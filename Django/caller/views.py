22286c0d6303acc1d81130eb75f9e3372cf3da75
JoshuaMukherjee <joshuaakmukherjee@gmail.com>
2021-02-25 09:24:25 +0000

    string

--- Django/caller/views.py
+++ Django/caller/views.py
@@ -31,7 +31,7 @@ def get_home(request):
             response = HttpResponseRedirect("/queue/")
             response.set_cookie("id", str(caller_id))
 
-            fname = data["firstname"].decode()
+            fname = data["firstname"]
             sname = data["surname"]
             dob = data["dob"]
 
