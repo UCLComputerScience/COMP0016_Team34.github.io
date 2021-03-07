48fb5715157bc0fff0944436b8e9667bf70ca528
ShaheerA2311 <ShaheerA2311@users.noreply.github.com>
2021-03-07 16:12:54 +0000

    css done

--- Django/caller/Form.py
+++ Django/caller/Form.py
@@ -1,12 +1,12 @@
 from django import forms
 
 class Dataform(forms.Form):
-    firstname = forms.CharField(label='first name')
-    surname = forms.CharField(label='surname')
-    dob = forms.CharField(label='date of birth DD/MM/YYYY')
+    firstname = forms.CharField(label='Firstname  ')
+    surname = forms.CharField(label='Surname  ')
+    dob = forms.CharField(label='Date of Birth (DD/MM/YYYY)  ')
 
 class IDform(forms.Form):
     id = forms.CharField(label='id')
 
 class DescForm(forms.Form):
-    desc = forms.CharField(label="description")
+    desc = forms.CharField(label="Description  ")
