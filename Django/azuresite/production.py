26191982c4e05e709b95d08d793d3f89da3f68f2
JoshuaMukherjee <joshuaakmukherjee@gmail.com>
2021-03-17 15:12:29 +0000

    production settings

--- Django/azuresite/production.py
+++ Django/azuresite/production.py
@@ -15,8 +15,11 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',                                 
     'django.contrib.auth.middleware.AuthenticationMiddleware',                   
     'django.contrib.messages.middleware.MessageMiddleware',                      
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',                    
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
+
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'  
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
