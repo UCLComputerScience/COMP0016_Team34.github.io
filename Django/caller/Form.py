b903af02884d6c8faa780cdaa1ca3c236d879630
ShaheerA2311 <ShaheerA2311@users.noreply.github.com>
2021-03-26 10:40:16 +0000

    change form

--- Django/caller/Form.py
+++ Django/caller/Form.py
@@ -3,7 +3,7 @@ from .models import *
 
 
 class Dataform(forms.Form):
-    firstname = forms.CharField(widget=forms.Textarea(attrs={'cols': 80, 'rows': 20}))
+    firstname = forms.CharField(widget=forms.Textarea(attrs={'id': 'first name', 'size': 'myfieldclass'}))
     
     surname = forms.CharField(label='Surname  ')
     dob = forms.CharField(label='Date of Birth (DD/MM/YYYY)  ')
@@ -12,4 +12,4 @@ class IDform(forms.Form):
     id = forms.CharField(label='id')
 
 class DescForm(forms.Form):
-    desc = forms.CharField(label="Description  ")
+    desc = forms.CharField(label="Description  ", widget=forms.Textarea(attrs={'cols': 80, 'rows': 10}))
